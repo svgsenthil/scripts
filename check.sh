a0cef173e2768d4237e3eb5bf9f31564edcc5c31
senthil <svgsenthil@gmail.com>
2019-05-03 11:41:39 +0530

    Add more packages for version check

--- check.sh
+++ check.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 echo "Checking Version for Installed Programs"
-ruby -v
-gem -v
-rails -v
+echo -n 'Git:            '
 git --version
+echo -n 'Node:           '
+node --version
+echo -n 'npm:            '
+npm --version
+echo -n 'Docker:         '
+docker --version
+echo -n 'Docker Compose: '
+docker-compose --version
+echo -n 'Python:         '
+python -V
+echo -n 'Ruby:           '
+ruby --version
+echo -n 'Rails:          '
+rails --version
+echo -n 'Gem:            '
+gem --version
